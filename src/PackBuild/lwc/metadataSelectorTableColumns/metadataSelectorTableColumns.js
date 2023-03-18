e1eba9874bcefbe905111af4646ff5584faf47f2
Christopher Pezza <christopher.pezza@ncino.com>
2023-03-18 11:33:57 -0400

    prettier clean up

--- src/PackBuild/lwc/metadataSelectorTableColumns/metadataSelectorTableColumns.js
+++ src/PackBuild/lwc/metadataSelectorTableColumns/metadataSelectorTableColumns.js
@@ -7,7 +7,13 @@ import Namespace_Column from "@salesforce/label/c.Namespace_Column";
 const MS_COLUMNS = [
   { label: Name_Column, fieldName: "fullName", type: "text", hideDefaultActions: true },
   { label: File_Name_Column, fieldName: "fileName", type: "text", hideDefaultActions: true },
-  { label: Manageable_State_Column, fieldName: "manageableState", type: "text", fixedWidth: 125, hideDefaultActions: true },
+  {
+    label: Manageable_State_Column,
+    fieldName: "manageableState",
+    type: "text",
+    fixedWidth: 125,
+    hideDefaultActions: true
+  },
   { label: Namespace_Column, fieldName: "namespacePrefix", type: "text", fixedWidth: 125, hideDefaultActions: true }
 ];
 
