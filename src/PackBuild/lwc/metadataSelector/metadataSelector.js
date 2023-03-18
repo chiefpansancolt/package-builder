ff07be0bd81bae9fdd9d2c249cdc780bd4b95b1b
Christopher Pezza <christopher.pezza@ncino.com>
2023-03-18 09:48:13 -0400

    Add dynamic Metadata query

--- src/PackBuild/lwc/metadataSelector/metadataSelector.js
+++ src/PackBuild/lwc/metadataSelector/metadataSelector.js
@@ -1,5 +1,5 @@
 /* eslint-disable @lwc/lwc/no-async-operation */
-import { LightningElement, track } from "lwc";
+import { LightningElement, track, wire } from "lwc";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
 /* Import Custom Utilities */
@@ -9,6 +9,7 @@ import { CONSTANTS } from "c/metadataSelectorUtilities";
 /* Import Class Methods */
 import listMetadata from "@salesforce/apex/MetadataSelector.listMetadata";
 import listFolders from "@salesforce/apex/MetadataSelector.listFolders";
+import getMetadataTypes from "@salesforce/apex/MetadataUtility.getMetadataTypes";
 
 /* Import Custom Labels */
 import Metadata_Type_Selector from "@salesforce/label/c.Metadata_Type_Selector";
@@ -42,6 +43,7 @@ export default class MetadataSelector extends LightningElement {
   @track metdataTypes = [];
   @track availableFolders = [];
   @track selectedMetadataTypes = [];
+  @track metadataOptions = [];
   @track columns = MS_COLUMNS;
   @track sfdxOutput = CONSTANTS.BLANK;
   @track selectedMetadataType = CONSTANTS.BLANK;
@@ -72,7 +74,7 @@ export default class MetadataSelector extends LightningElement {
   @track metadataTypeSetting = {
     title: Metadata_Type_Selector,
     iconName: CONSTANTS.ICONS.METADATATYPE,
-    isLoading: false,
+    isLoading: true,
     isEmpty: false,
     hideFooter: false,
     emptyText: CONSTANTS.BLANK
@@ -107,6 +109,17 @@ export default class MetadataSelector extends LightningElement {
     emptyText: CONSTANTS.BLANK,
     show: false
   };
+
+  @wire(getMetadataTypes)
+  wiredGetMetadataTypes({ error, data}) {
+    if (data) {
+      this.metadataOptions = JSON.parse(data);
+      this.metadataTypeSetting.isLoading = false;
+    } else if (error) {
+      this.showNotification(0, error, CONSTANTS.BLANK, CONSTANTS.BLANK);
+      this.metadataTypeSetting.isLoading = false;
+    }
+  }
 
   handleSfdxCopyCode(event) {
     this.template.querySelector("c-sfdx-code-snippet").handleCopy();
@@ -257,218 +270,6 @@ export default class MetadataSelector extends LightningElement {
       { label: Package_Type_All, value: "all" },
       { label: Package_Type_Unmanaged, value: "unmanagedOnly" },
       { label: Package_Type_Managed, value: "managedOnly" }
-    ];
-  }
-
-  get metadataOptions() {
-    return [
-      { label: "Action Link Group Template", value: "ActionLinkGroupTemplate" },
-      { label: "Action Override", value: "ActionOverride" },
-      { label: "Analytic Snapshot", value: "AnalyticSnapshot" },
-      { label: "Apex Class", value: "ApexClass" },
-      { label: "Apex Component", value: "ApexComponent" },
-      { label: "Apex Page", value: "ApexPage" },
-      { label: "Apex Trigger", value: "ApexTrigger" },
-      { label: "App Menu", value: "AppMenu" },
-      { label: "Approval Process", value: "ApprovalProcess" },
-      { label: "Article Type", value: "ArticleType" },
-      { label: "Assignment Rules", value: "AssignmentRules" },
-      { label: "Audience", value: "Audience" },
-      { label: "Auth Provider", value: "AuthProvider" },
-      { label: "Aura Definition Bundle", value: "AuraDefinitionBundle" },
-      { label: "Auto Response Rules", value: "AutoResponseRules" },
-      { label: "Base Sharing Rule", value: "BaseSharingRule" },
-      { label: "Bot", value: "Bot" },
-      { label: "Bot Version", value: "BotVersion" },
-      { label: "Branding Set", value: "BrandingSet" },
-      { label: "Business Process", value: "BusinessProcess" },
-      { label: "Call Center", value: "CallCenter" },
-      { label: "Case Subject Particle", value: "CaseSubjectParticle" },
-      { label: "Certificate", value: "Certificate" },
-      { label: "Chatter Extension", value: "ChatterExtension" },
-      { label: "Clean Data Service", value: "CleanDataService" },
-      { label: "CMS Connect Source", value: "CMSConnectSource" },
-      { label: "Compact Layout", value: "CompactLayout" },
-      { label: "Community", value: "Community" },
-      {
-        label: "Community Template Definition",
-        value: "CommunityTemplateDefinition"
-      },
-      {
-        label: "Community Theme Definition",
-        value: "CommunityThemeDefinition"
-      },
-      { label: "Connected App", value: "ConnectedApp" },
-      { label: "Content Asset", value: "ContentAsset" },
-      { label: "Cors Whitelist Origin", value: "CorsWhitelistOrigin" },
-      {
-        label: "Criteria Based Sharing Rule",
-        value: "CriteriaBasedSharingRule"
-      },
-      { label: "Csp Trusted Site", value: "CspTrustedSite" },
-      { label: "Custom Application", value: "CustomApplication" },
-      {
-        label: "Custom Application Component",
-        value: "CustomApplicationComponent"
-      },
-      { label: "Custom Feed Filter", value: "CustomFeedFilter" },
-      { label: "Custom Field", value: "CustomField" },
-      { label: "Custom Help Menu Section", value: "CustomHelpMenuSection" },
-      { label: "Custom Labels", value: "CustomLabels" },
-      { label: "Custom Label", value: "CustomLabel" },
-      { label: "Custom Metadata Types", value: "CustomMetadataTypes" },
-      { label: "Custom Metadata", value: "CustomMetadata" },
-      { label: "Custom Object", value: "CustomObject" },
-      { label: "Custom Object Translation", value: "CustomObjectTranslation" },
-      { label: "Custom Page Web Link", value: "CustomPageWebLink" },
-      { label: "Custom Permission", value: "CustomPermission" },
-      { label: "Custom Site", value: "CustomSite" },
-      { label: "Custom Tab", value: "CustomTab" },
-      { label: "Dashboard", value: "Dashboard" },
-      { label: "Data Category Group", value: "DataCategoryGroup" },
-      { label: "Delegate Group", value: "DelegateGroup" },
-      { label: "Document", value: "Document" },
-      { label: "Duplicate Rule", value: "DuplicateRule" },
-      { label: "Eclair Geo Data", value: "EclairGeoData" },
-      { label: "Email Services Function", value: "EmailServicesFunction" },
-      { label: "Email Template", value: "EmailTemplate" },
-      { label: "Embedded Service Branding", value: "EmbeddedServiceBranding" },
-      { label: "Embedded Service Config", value: "EmbeddedServiceConfig" },
-      {
-        label: "Embedded Service Flow Config",
-        value: "EmbeddedServiceFlowConfig"
-      },
-      {
-        label: "Embedded Service Live Agent",
-        value: "EmbeddedServiceLiveAgent"
-      },
-      { label: "Entitlement Process", value: "EntitlementProcess" },
-      { label: "Entitlement Template", value: "EntitlementTemplate" },
-      { label: "Event Delivery", value: "EventDelivery" },
-      { label: "Event Subscription", value: "EventSubscription" },
-      {
-        label: "External Service Registration",
-        value: "ExternalServiceRegistration"
-      },
-      { label: "External Data Source", value: "ExternalDataSource" },
-      { label: "Feature Parameter Boolean", value: "FeatureParameterBoolean" },
-      { label: "Feature Parameter Date", value: "FeatureParameterDate" },
-      { label: "Feature Parameter Integer", value: "FeatureParameterInteger" },
-      { label: "Field Set", value: "FieldSet" },
-      { label: "Flexi Page", value: "FlexiPage" },
-      { label: "Flow", value: "Flow" },
-      { label: "Flow Category", value: "FlowCategory" },
-      { label: "Flow Definition", value: "FlowDefinition" },
-      { label: "Folder", value: "Folder" },
-      { label: "Folder Share", value: "FolderShare" },
-      { label: "Global Value Set", value: "GlobalValueSet" },
-      {
-        label: "Global Value Set Translation",
-        value: "GlobalValueSetTranslation"
-      },
-      { label: "Global Picklist Value", value: "GlobalPicklistValue" },
-      { label: "Group", value: "Group" },
-      { label: "Home Page Component", value: "HomePageComponent" },
-      { label: "Home Page Layout", value: "HomePageLayout" },
-      { label: "Index", value: "Index" },
-      { label: "Installed Package", value: "InstalledPackage" },
-      { label: "Keyword List", value: "KeywordList" },
-      { label: "Layout", value: "Layout" },
-      { label: "Letterhead", value: "Letterhead" },
-      { label: "Lightning Bolt", value: "LightningBolt" },
-      {
-        label: "Lightning Component Bundle",
-        value: "LightningComponentBundle"
-      },
-      {
-        label: "Lightning Experience Theme",
-        value: "LightningExperienceTheme"
-      },
-      { label: "List View", value: "ListView" },
-      { label: "Live Chat Agent Config", value: "LiveChatAgentConfig" },
-      { label: "Live Chat Button", value: "LiveChatButton" },
-      { label: "Live Chat Deployment", value: "LiveChatDeployment" },
-      {
-        label: "Live Chat Sensitive Data Rule",
-        value: "LiveChatSensitiveDataRule"
-      },
-      { label: "Managed Topics", value: "ManagedTopics" },
-      { label: "Matching Rule", value: "MatchingRule" },
-      { label: "Metadata", value: "Metadata" },
-      { label: "Metadata With Content", value: "MetadataWithContent" },
-      { label: "Milestone Type", value: "MilestoneType" },
-      { label: "MlDomain", value: "MlDomain" },
-      { label: "Moderation Rule", value: "ModerationRule" },
-      { label: "Named Credential", value: "NamedCredential" },
-      { label: "Named Filter", value: "NamedFilter" },
-      { label: "Network", value: "Network" },
-      { label: "Network Branding", value: "NetworkBranding" },
-      { label: "Owner Sharing Rule", value: "OwnerSharingRule" },
-      { label: "Package", value: "Package" },
-      { label: "Path Assistant", value: "PathAssistant" },
-      { label: "Permission Set", value: "PermissionSet" },
-      { label: "Picklist", value: "Picklist" },
-      { label: "Platform Cache Partition", value: "PlatformCachePartition" },
-      { label: "Platform Event Channel", value: "PlatformEventChannel" },
-      { label: "Portal", value: "Portal" },
-      { label: "Post Template", value: "PostTemplate" },
-      { label: "Presence Decline Reason", value: "PresenceDeclineReason" },
-      { label: "Presence User Config", value: "PresenceUserConfig" },
-      { label: "Profile", value: "Profile" },
-      { label: "Profile Action Override", value: "ProfileActionOverride" },
-      { label: "Profile Password Policy", value: "ProfilePasswordPolicy" },
-      { label: "Profile Session Setting", value: "ProfileSessionSetting" },
-      { label: "Queue", value: "Queue" },
-      { label: "Queue Routing Config", value: "QueueRoutingConfig" },
-      { label: "Quick Action", value: "QuickAction" },
-      { label: "Recommendation Strategy", value: "RecommendationStrategy" },
-      { label: "Record Action Deployment", value: "RecordActionDeployment" },
-      { label: "Record Type", value: "RecordType" },
-      { label: "Remote Site Setting", value: "RemoteSiteSetting" },
-      { label: "Report", value: "Report" },
-      { label: "Report Type", value: "ReportType" },
-      { label: "Role", value: "Role" },
-      { label: "Saml SSO Config", value: "SamlSsoConfig" },
-      { label: "Scontrol", value: "Scontrol" },
-      { label: "Search Layouts", value: "SearchLayouts" },
-      { label: "Service Channel", value: "ServiceChannel" },
-      { label: "Service Presence Status", value: "ServicePresenceStatus" },
-      { label: "Settings", value: "Settings" },
-      { label: "Sharing Base Rule", value: "SharingBaseRule" },
-      { label: "Sharing Reason", value: "SharingReason" },
-      { label: "Sharing Recalculation", value: "SharingRecalculation" },
-      { label: "Sharing Rules", value: "SharingRules" },
-      { label: "Sharing Set", value: "SharingSet" },
-      { label: "Site Dot Com", value: "SiteDotCom" },
-      { label: "Skill", value: "Skill" },
-      { label: "Standard Value Set", value: "StandardValueSet" },
-      {
-        label: "Standard Value Set Translation",
-        value: "StandardValueSetTranslation"
-      },
-      { label: "Static Resource", value: "StaticResource" },
-      { label: "Synonym Dictionary", value: "SynonymDictionary" },
-      { label: "Territory", value: "Territory" },
-      { label: "Territory2", value: "Territory2" },
-      { label: "Territory2 Model", value: "Territory2Model" },
-      { label: "Territory2 Rule", value: "Territory2Rule" },
-      { label: "Territory2 Type", value: "Territory2Type" },
-      { label: "Topics For Objects", value: "TopicsForObjects" },
-      {
-        label: "Transaction Security Policy",
-        value: "TransactionSecurityPolicy"
-      },
-      { label: "Translations", value: "Translations" },
-      { label: "Validation Rule", value: "ValidationRule" },
-      { label: "Wave Application", value: "WaveApplication" },
-      { label: "Wave Dashboard", value: "WaveDashboard" },
-      { label: "Wave Dataflow", value: "WaveDataflow" },
-      { label: "Wave Dataset", value: "WaveDataset" },
-      { label: "Wave Lens", value: "WaveLens" },
-      { label: "Wave Template Bundle", value: "WaveTemplateBundle" },
-      { label: "Wave Xmd", value: "WaveXmd" },
-      { label: "Web Link", value: "WebLink" },
-      { label: "Workflow", value: "Workflow" }
     ];
   }
 
