899c6ce0f7b53a901866cc59a17ddb798c81cd6f
Christopher Pezza <chris.pezza@hey.com>
2024-11-18 13:00:04 -0500

    feat: implement labelService

---
+++ src/PackBuild/lwc/labelService/labelService.js
@@ -0,0 +1,59 @@
+import Metadata_Type_Selector from "@salesforce/label/c.Metadata_Type_Selector";
+import Metadata_Types from "@salesforce/label/c.Metadata_Types";
+import Folders from "@salesforce/label/c.Folders";
+import Metadata_Types_Placeholder from "@salesforce/label/c.Metadata_Types_Placeholder";
+import Folders_Placeholder from "@salesforce/label/c.Folders_Placeholder";
+import Package_Types_Placeholder from "@salesforce/label/c.Package_Types_Placeholder";
+import Metadata_Types_Missing from "@salesforce/label/c.Metadata_Types_Missing";
+import Folders_Missing from "@salesforce/label/c.Folders_Missing";
+import Package_Types_Missing from "@salesforce/label/c.Package_Types_Missing";
+import Search_Button from "@salesforce/label/c.Search_Button";
+import Results_Title from "@salesforce/label/c.Results_Title";
+import Package_Title from "@salesforce/label/c.Package_Title";
+import SFDX_Retrieve_Title from "@salesforce/label/c.SFDX_Retrieve_Title";
+import Package_Type_All from "@salesforce/label/c.Package_Type_All";
+import Package_Type_Unmanaged from "@salesforce/label/c.Package_Type_Unmanaged";
+import Package_Type_Managed from "@salesforce/label/c.Package_Type_Managed";
+import Metadata_Retrieve_Error_Title from "@salesforce/label/c.Metadata_Retrieve_Error_Title";
+import Metadata_Retrieve_Success_Message from "@salesforce/label/c.Metadata_Retrieve_Success_Message";
+import Package_Types from "@salesforce/label/c.Package_Types";
+import Metadata_Retrieve_Success_Title from "@salesforce/label/c.Metadata_Retrieve_Success_Title";
+import Invalid_Metadata_Types from "@salesforce/label/c.Invalid_Metadata_Types";
+import Invalid_Package_Types from "@salesforce/label/c.Invlid_Package_Types";
+import Copy_Button_Label from "@salesforce/label/c.Copy_Button_Label";
+import Copy_All_Button_Label from "@salesforce/label/c.Copy_All_Button_Label";
+import Copy_Types_Button_Label from "@salesforce/label/c.Copy_Types_Button_Label";
+import Copied_Button_Label from "@salesforce/label/c.Copied_Button_Label";
+import Available_Folders_Label from "@salesforce/label/c.Available_Folders_Label";
+import Selected_Folders_Label from "@salesforce/label/c.Selected_Folders_Label";
+
+export const labels = {
+	Metadata_Type_Selector,
+	Metadata_Types,
+	Folders,
+	Metadata_Types_Placeholder,
+	Folders_Placeholder,
+	Package_Types_Placeholder,
+	Metadata_Types_Missing,
+	Folders_Missing,
+	Package_Types_Missing,
+	Search_Button,
+	Results_Title,
+	Package_Title,
+	SFDX_Retrieve_Title,
+	Package_Type_All,
+	Package_Type_Unmanaged,
+	Package_Type_Managed,
+	Metadata_Retrieve_Error_Title,
+	Metadata_Retrieve_Success_Message,
+	Package_Types,
+	Metadata_Retrieve_Success_Title,
+	Invalid_Metadata_Types,
+	Invalid_Package_Types,
+	Copy_Button_Label,
+	Copy_All_Button_Label,
+	Copy_Types_Button_Label,
+	Copied_Button_Label,
+	Available_Folders_Label,
+	Selected_Folders_Label,
+}
--- src/PackBuild/lwc/metadataSelector/metadataSelector.js
+++ src/PackBuild/lwc/metadataSelector/metadataSelector.js
@@ -5,6 +5,7 @@ import { ShowToastEvent } from "lightning/platformShowToastEvent";
 /* Import Custom Utilities */
 import { MS_COLUMNS } from "c/metadataSelectorTableColumns";
 import { CONSTANTS } from "c/metadataSelectorUtilities";
+import { labels } from "c/labelService";
 
 /* Import Class Methods */
 import APIVERSION from "@salesforce/apex/MetadataUtility.APIVERSION";
@@ -13,72 +14,24 @@ import listMetadata from "@salesforce/apex/MetadataSelector.listMetadata";
 import listFolders from "@salesforce/apex/MetadataSelector.listFolders";
 import getMetadataTypes from "@salesforce/apex/MetadataUtility.getMetadataTypes";
 
-/* Import Custom Labels */
-import Metadata_Type_Selector from "@salesforce/label/c.Metadata_Type_Selector";
-import Metadata_Types from "@salesforce/label/c.Metadata_Types";
-import Folders from "@salesforce/label/c.Folders";
-import Metadata_Types_Placeholder from "@salesforce/label/c.Metadata_Types_Placeholder";
-import Folders_Placeholder from "@salesforce/label/c.Folders_Placeholder";
-import Package_Types_Placeholder from "@salesforce/label/c.Package_Types_Placeholder";
-import Metadata_Types_Missing from "@salesforce/label/c.Metadata_Types_Missing";
-import Folders_Missing from "@salesforce/label/c.Folders_Missing";
-import Package_Types_Missing from "@salesforce/label/c.Package_Types_Missing";
-import Search_Button from "@salesforce/label/c.Search_Button";
-import Results_Title from "@salesforce/label/c.Results_Title";
-import Package_Title from "@salesforce/label/c.Package_Title";
-import SFDX_Retrieve_Title from "@salesforce/label/c.SFDX_Retrieve_Title";
-import Package_Type_All from "@salesforce/label/c.Package_Type_All";
-import Package_Type_Unmanaged from "@salesforce/label/c.Package_Type_Unmanaged";
-import Package_Type_Managed from "@salesforce/label/c.Package_Type_Managed";
-import Metadata_Retrieve_Error_Title from "@salesforce/label/c.Metadata_Retrieve_Error_Title";
-import Metadata_Retrieve_Success_Message from "@salesforce/label/c.Metadata_Retrieve_Success_Message";
-import Package_Types from "@salesforce/label/c.Package_Types";
-import Metadata_Retrieve_Success_Title from "@salesforce/label/c.Metadata_Retrieve_Success_Title";
-import Invalid_Metadata_Types from "@salesforce/label/c.Invalid_Metadata_Types";
-import Invalid_Package_Types from "@salesforce/label/c.Invlid_Package_Types";
-import Copy_Button_Label from "@salesforce/label/c.Copy_Button_Label";
-import Copy_All_Button_Label from "@salesforce/label/c.Copy_All_Button_Label";
-import Copy_Types_Button_Label from "@salesforce/label/c.Copy_Types_Button_Label";
-import Copied_Button_Label from "@salesforce/label/c.Copied_Button_Label";
-import Available_Folders_Label from "@salesforce/label/c.Available_Folders_Label";
-import Selected_Folders_Label from "@salesforce/label/c.Selected_Folders_Label";
-
 export default class MetadataSelector extends LightningElement {
-  @track metadataTypes = [];
-  @track availableFolders = [];
-  @track selectedMetadataTypes = [];
-  @track metadataOptions = [];
-  @track columns = MS_COLUMNS;
-  @track sfdxOutput = CONSTANTS.BLANK;
-  @track selectedMetadataType = CONSTANTS.BLANK;
-  @track selectedPackageType = CONSTANTS.BLANK;
-  @track selectedFolders = [];
-  @track showMetadataList = false;
-  @track showFolderList = false;
-  @track showPackageList = false;
-  @track includeAllSymbol = false;
-
-  labels = {
-    Metadata_Types,
-    Folders,
-    Package_Types,
-    Metadata_Types_Placeholder,
-    Folders_Placeholder,
-    Package_Types_Placeholder,
-    Metadata_Types_Missing,
-    Folders_Missing,
-    Package_Types_Missing,
-    Search_Button,
-    Metadata_Retrieve_Success_Title,
-    Copy_Button_Label,
-    Copy_All_Button_Label,
-    Copy_Types_Button_Label,
-    Available_Folders_Label,
-    Selected_Folders_Label
-  };
+  metadataTypes = [];
+  availableFolders = [];
+  selectedMetadataTypes = [];
+  metadataOptions = [];
+  columns = MS_COLUMNS;
+  sfdxOutput = CONSTANTS.BLANK;
+  selectedMetadataType = CONSTANTS.BLANK;
+  selectedPackageType = CONSTANTS.BLANK;
+  selectedFolders = [];
+  showMetadataList = false;
+  showFolderList = false;
+  showPackageList = false;
+  includeAllSymbol = false;
+  labels = labels;
 
   @track metadataTypeSetting = {
-    title: Metadata_Type_Selector,
+    title: labels.Metadata_Type_Selector,
     iconName: CONSTANTS.ICONS.METADATATYPE,
     isLoading: true,
     isEmpty: false,
@@ -87,7 +40,7 @@ export default class MetadataSelector extends LightningElement {
   };
 
   @track metadataListSetting = {
-    title: Results_Title,
+    title: labels.Results_Title,
     iconName: CONSTANTS.ICONS.METADATALIST,
     isLoading: false,
     isEmpty: false,
@@ -97,7 +50,7 @@ export default class MetadataSelector extends LightningElement {
   };
 
   @track packageOutputSetting = {
-    title: Package_Title,
+    title: labels.Package_Title,
     iconName: CONSTANTS.ICONS.PACKAGE,
     isLoading: false,
     isEmpty: false,
@@ -107,7 +60,7 @@ export default class MetadataSelector extends LightningElement {
   };
 
   @track sfdxOutputSetting = {
-    title: SFDX_Retrieve_Title,
+    title: labels.SFDX_Retrieve_Title,
     iconName: CONSTANTS.ICONS.SFDX,
     isLoading: false,
     isEmpty: false,
@@ -144,30 +97,30 @@ export default class MetadataSelector extends LightningElement {
   handleSfdxCopyCode(event) {
     this.template.querySelector("c-sfdx-code-snippet").handleCopy();
     let button = event.target;
-    button.label = Copied_Button_Label;
+    button.label = labels.Copied_Button_Label;
 
     setTimeout(() => {
-      button.label = Copy_Button_Label;
+      button.label = labels.Copy_Button_Label;
     }, 1000);
   }
 
   handlePackageCopyCodeAll(event) {
     this.template.querySelector("c-package-code-snippet").handleCopyAll();
     let button = event.target;
-    button.label = Copied_Button_Label;
+    button.label = labels.Copied_Button_Label;
 
     setTimeout(() => {
-      button.label = Copy_All_Button_Label;
+      button.label = labels.Copy_All_Button_Label;
     }, 1000);
   }
 
   handlePackageCopyCodeType(event) {
     this.template.querySelector("c-package-code-snippet").handleCopyTypes();
     let button = event.target;
-    button.label = Copied_Button_Label;
+    button.label = labels.Copied_Button_Label;
 
     setTimeout(() => {
-      button.label = Copy_Types_Button_Label;
+      button.label = labels.Copy_Types_Button_Label;
     }, 1000);
   }
 
@@ -198,7 +151,7 @@ export default class MetadataSelector extends LightningElement {
           this.availableFolders = [];
           this.selectedFolder = "";
 
-          this.showNotification(0, error, CONSTANTS.BLANK, Metadata_Retrieve_Error_Title);
+          this.showNotification(0, error, CONSTANTS.BLANK, labels.Metadata_Retrieve_Error_Title);
           this.metadataTypeSetting.isLoading = false;
         });
     } else {
@@ -220,11 +173,11 @@ export default class MetadataSelector extends LightningElement {
   handleMetadataSearch() {
     this.metadataTypeSetting.isLoading = true;
     if (!this.search(this.selectedMetadataType, this.metadataOptions)) {
-      this.message = Invalid_Metadata_Types;
+      this.message = labels.Invalid_Metadata_Types;
     }
 
     if (!this.search(this.selectedPackageType, this.packageTypeOptions)) {
-      this.message = Invalid_Package_Types;
+      this.message = labels.Invalid_Package_Types;
     }
 
     listMetadata({
@@ -246,14 +199,14 @@ export default class MetadataSelector extends LightningElement {
           this.includeAllSymbol = false;
         }
 
-        this.showNotification(2, CONSTANTS.BLANK, Metadata_Retrieve_Success_Message, Metadata_Retrieve_Success_Title);
+        this.showNotification(2, CONSTANTS.BLANK, labels.Metadata_Retrieve_Success_Message, labels.Metadata_Retrieve_Success_Title);
         this.metadataTypeSetting.isLoading = false;
       })
       .catch((error) => {
         this.data = undefined;
         this.includeAllSymbol = false;
 
-        this.showNotification(0, error, CONSTANTS.BLANK, Metadata_Retrieve_Error_Title);
+        this.showNotification(0, error, CONSTANTS.BLANK, labels.Metadata_Retrieve_Error_Title);
         this.metadataTypeSetting.isLoading = false;
       });
   }
@@ -263,7 +216,11 @@ export default class MetadataSelector extends LightningElement {
     this.sfdxOutput = CONSTANTS.BLANK;
 
     this.selectedMetadataTypes.forEach((element) => {
-      this.sfdxOutput += this.selectedMetadataType + ":" + element.fullName + ",";
+      if (this.CLI_COMMAND == 'sf') {
+        this.sfdxOutput += " -m " + this.selectedMetadataType + ":" + element.fullName;
+      } else {
+        this.sfdxOutput += this.selectedMetadataType + ":" + element.fullName + ",";
+      }
     });
 
     this.sfdxOutput = this.sfdxOutput.slice(0, -1);
@@ -287,9 +244,9 @@ export default class MetadataSelector extends LightningElement {
 
   get packageTypeOptions() {
     return [
-      { label: Package_Type_All, value: "all" },
-      { label: Package_Type_Unmanaged, value: "unmanagedOnly" },
-      { label: Package_Type_Managed, value: "managedOnly" }
+      { label: labels.Package_Type_All, value: "all" },
+      { label: labels.Package_Type_Unmanaged, value: "unmanagedOnly" },
+      { label: labels.Package_Type_Managed, value: "managedOnly" }
     ];
   }
 
@@ -313,7 +270,7 @@ export default class MetadataSelector extends LightningElement {
 
   showNotification(type, error, msg, title) {
     const evt = new ShowToastEvent({
-      title: title !== CONSTANTS.BLANK ? title : CONSTANTS.VARIANTOPTIONS[type].label,
+      title: labels.title !== CONSTANTS.BLANK ? title : CONSTANTS.VARIANTOPTIONS[type].label,
       message: type === 0 ? error.body.message : msg,
       variant: CONSTANTS.VARIANTOPTIONS[type].value,
       mode: type === 0 ? "sticky" : "dismissible"
