a5a1418aa627a121927830825477005e3216dc4e
Christopher Pezza <chris.pezza@hey.com>
2025-01-02 20:33:34 -0500

    chore: prettier clean up

--- src/PackBuild/lwc/metadataSelectorUtilities/metadataSelectorUtilities.js
+++ src/PackBuild/lwc/metadataSelectorUtilities/metadataSelectorUtilities.js
@@ -1,25 +1,25 @@
 const CONSTANTS = {
-  APIVERSION: "62.0",
-  ICONS: {
-    METADATATYPE: "custom:custom63",
-    METADATALIST: "standard:related_list",
-    PACKAGE: "standard:file",
-    SFDX: "standard:file"
-  },
-  BLANK: "",
-  DEFAULT_MESSAGE: "Message",
-  VARIANTOPTIONS: [
-    { label: "Error", value: "error" },
-    { label: "Warning", value: "warning" },
-    { label: "Success", value: "success" },
-    { label: "Info", value: "info" }
-  ]
+	APIVERSION: '62.0',
+	ICONS: {
+		METADATATYPE: 'custom:custom63',
+		METADATALIST: 'standard:related_list',
+		PACKAGE: 'standard:file',
+		SFDX: 'standard:file'
+	},
+	BLANK: '',
+	DEFAULT_MESSAGE: 'Message',
+	VARIANTOPTIONS: [
+		{ label: 'Error', value: 'error' },
+		{ label: 'Warning', value: 'warning' },
+		{ label: 'Success', value: 'success' },
+		{ label: 'Info', value: 'info' }
+	]
 };
 
 const copy = async (textToCopy) =>
-  navigator.clipboard.writeText(textToCopy).catch(
-    (err) => console.error(JSON.stringify(err)),
-    (err) => console.error(JSON.stringify(err))
-  );
+	navigator.clipboard.writeText(textToCopy).catch(
+		(err) => console.error(JSON.stringify(err)),
+		(err) => console.error(JSON.stringify(err))
+	);
 
 export { CONSTANTS, copy };
--- src/PackBuild/lwc/packageCodeSnippet/packageCodeSnippet.js
+++ src/PackBuild/lwc/packageCodeSnippet/packageCodeSnippet.js
@@ -1,21 +1,21 @@
-import { LightningElement, api } from "lwc";
+import { LightningElement, api } from 'lwc';
 
 /* Import Custom Utilities */
-import { CONSTANTS, copy } from "c/metadataSelectorUtilities";
+import { CONSTANTS, copy } from 'c/metadataSelectorUtilities';
 
 export default class PackageCodeSnippet extends LightningElement {
-  @api includeAllSymbol = false;
-  @api selectedMetadataTypes;
-  @api selectedMetadataType;
+	@api includeAllSymbol = false;
+	@api selectedMetadataTypes;
+	@api selectedMetadataType;
 
-  @api
-  async handleCopyAll() {
-    let members = "";
-    for (let i = 0; i < this.selectedMetadataTypes.length; i++) {
-      members += "    <members>" + this.selectedMetadataTypes[i].fullName + "</members>\n";
-    }
-    members.replace(/\n$/, "");
-    const code = `<?xml version="1.0" encoding="UTF-8"?>
+	@api
+	async handleCopyAll() {
+		let members = '';
+		for (let i = 0; i < this.selectedMetadataTypes.length; i++) {
+			members += '    <members>' + this.selectedMetadataTypes[i].fullName + '</members>\n';
+		}
+		members.replace(/\n$/, '');
+		const code = `<?xml version="1.0" encoding="UTF-8"?>
 <Package xmlns="http://soap.sforce.com/2006/04/metadata">
   <types>
 ${members}    <name>${this.selectedMetadataType}</name>
@@ -23,55 +23,55 @@ ${members}    <name>${this.selectedMetadataType}</name>
   <version>56.0</version>
 </Package>`;
 
-    const input = document.createElement("textarea");
-    input.value = code;
+		const input = document.createElement('textarea');
+		input.value = code;
 
-    document.body.appendChild(input);
-    input.select();
+		document.body.appendChild(input);
+		input.select();
 
-    if (navigator.clipboard) {
-      console.log("clipy");
-      const selection = document.getSelection();
-      await copy(selection.toString());
-    } else {
-      document.execCommand("copy");
-    }
-    document.body.removeChild(input);
-  }
+		if (navigator.clipboard) {
+			console.log('clipy');
+			const selection = document.getSelection();
+			await copy(selection.toString());
+		} else {
+			document.execCommand('copy');
+		}
+		document.body.removeChild(input);
+	}
 
-  @api
-  async handleCopyTypes() {
-    let members = "";
-    for (let i = 0; i < this.selectedMetadataTypes.length; i++) {
-      members += "    <members>" + this.selectedMetadataTypes[i].fullName + "</members>\n";
-    }
-    members.replace(/\n$/, "");
-    const code = `<types>
+	@api
+	async handleCopyTypes() {
+		let members = '';
+		for (let i = 0; i < this.selectedMetadataTypes.length; i++) {
+			members += '    <members>' + this.selectedMetadataTypes[i].fullName + '</members>\n';
+		}
+		members.replace(/\n$/, '');
+		const code = `<types>
 ${members}    <name>${this.selectedMetadataType}</name>
   </types>`;
 
-    const input = document.createElement("textarea");
-    input.value = code;
+		const input = document.createElement('textarea');
+		input.value = code;
 
-    document.body.appendChild(input);
-    input.select();
+		document.body.appendChild(input);
+		input.select();
 
-    if (navigator.clipboard) {
-      console.log("clipy");
-      const selection = document.getSelection();
-      await copy(selection.toString());
-    } else {
-      document.execCommand("copy");
-    }
-    document.body.removeChild(input);
-  }
+		if (navigator.clipboard) {
+			console.log('clipy');
+			const selection = document.getSelection();
+			await copy(selection.toString());
+		} else {
+			document.execCommand('copy');
+		}
+		document.body.removeChild(input);
+	}
 
-  labels = {
-    apiversion: CONSTANTS.APIVERSION,
-    name: "name",
-    version: "version",
-    types: "types",
-    members: "members",
-    package: "Package"
-  };
+	labels = {
+		apiversion: CONSTANTS.APIVERSION,
+		name: 'name',
+		version: 'version',
+		types: 'types',
+		members: 'members',
+		package: 'Package'
+	};
 }
